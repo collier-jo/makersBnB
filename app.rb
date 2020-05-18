0490206dd21e82f3cb937b71ec87baf7b7cbd652
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-18 17:01:53 +0100

    created lib and views folders

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require 'sinatra/base'
 class MakersBnB < Sinatra::Base
 
   get '/signup' do
-    'Sign Up'
+    erb(:signup)
+  end
+
+  post '/register' do
+    p params
   end
 
   run! if app_file == $0
