f74c70432510d2c5bd83c1fd87eb1cc0e86d65d4
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-18 16:52:13 +0100

    sign up form added

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,5 +3,9 @@ feature 'User sign up' do
     visit('/signup')
 
     expect(page).to have_content "Sign Up"
+    fill_in('username', with: 'user1')
+    fill_in('email', with: 'email@email.com')
+    fill_in('password', with: 'password123')
+    click_button('Register')
   end
 end
