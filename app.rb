81d28d180acc1831f6c30270e142f2660231da0f
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-05-19 11:51:47 +0100

    Flash added

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
 require './database_connection_setup'
 require './lib/user'
+require 'sinatra/flash'
 
 class MakersBnB < Sinatra::Base
   enable :sessions
+  register Sinatra::Flash
 
   get '/' do
     @user = User.find(id: session[:user_id])
@@ -18,6 +20,7 @@ class MakersBnB < Sinatra::Base
       user = User.create(email: params[:email], username: params[:username], password: params[:password])
     rescue PG::UniqueViolation
       redirect '/signup'
+      flash[:notice] = "this username/email already exists"
     end
     session[:user_id] = user.id
     redirect '/'
