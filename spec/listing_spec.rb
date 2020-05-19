056294e6f6f32b858b1ca47151a472f3e9e152c3
joannacollier <collj035@gmail.com>
2020-05-19 11:07:50 +0100

    Feature test working with database

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -8,7 +8,7 @@ describe Listing do
     
       connection.exec("INSERT INTO listings (name, description, price) VALUES('Minerva', 'A tiny house in the middle nowhere', 10.50);")
       listings = Listing.all
-      expect(listings).to include("Minerva")
+      expect(listings).to include "Minerva"
       expect(listings).to include "A tiny house in the middle nowhere"
       expect(listings).to include "10.50"
     end
