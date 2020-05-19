b298ef3490e4c70ac3d4764a25fd35a4d013c0d5
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-19 12:32:31 +0100

    added feature test for flash error message

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,4 +8,18 @@ feature 'User sign up' do
     fill_in('password', with: 'password123')
     click_button('Register')
   end
+
+  scenario 'An email/username already exists' do
+    user = User.create(
+      email: 'email@email.com',
+      password: 'password123',
+      username: 'user1'
+    )
+    visit('/signup')
+    fill_in('username', with: 'user1')
+    fill_in('email', with: 'email@email.com')
+    fill_in('password', with: 'password123')
+    click_button('Register')
+    expect(page).to have_content "this username/email already exists"
+  end
 end
