d178657855a4ab408503e25c84a41a91ffd5b828
LTurns <lizzieturney@gmail.com>
2020-05-22 14:42:14 +0100

    booking table started

--- spec/features/confirm_booking_spec.rb
+++ spec/features/confirm_booking_spec.rb
@@ -5,9 +5,9 @@ scenario 'user can see their booking dates' do
   user_sign_in
   listing = Listing.create(name: "House of Horrors", description: "A very scary house", price: "69.85")
   Available_Dates.create(listing_id: listing.id, date_start: '2020-05-04' , date_end: '2020-05-05');
-  Booking.create(listing_id: "#{listing.id}", user_id: "#{User.current_user.id}", book_from: "2020-05-04", book_to: "2020-05-05")
   visit("/booking/#{listing.id}/book")
 
+  Booking.create(listing_id: "#{listing.id}", user_id: "#{User.current_user.id}", book_from: "2020-05-04", book_to: "2020-05-05")
   expect(page).to have_button "Submit"
   click_button "Submit"
   expect(current_path).to eq "/booking/confirmation"
